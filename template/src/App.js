ebaafb9934422fcd36cd9caff48e963fd1d2b773
Agney <agney.menon@bigbinary.com>
2021-01-22 23:45:54 +0530

    fix logo height

--- template/src/App.js
+++ template/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="text-center">
       <header className="bg-gray-800 text-white flex flex-col items-center justify-center min-h-screen text-3xl">
-        <img src={logo} className="pointer-events-none h-40min animate-spin" alt="logo" />
+        <img src={logo} className="pointer-events-none h-40 animate-spin" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
